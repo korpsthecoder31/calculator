9528ef21b2e0a8c0c7a23e7f4c64dee8e6386070
korpsthecoder31 <korpsthewriter@gmail.com>
2024-10-29 07:44:31 +0800

    feat: create function for basic math operations
        - create 4 functions for each of the following:
            - add
            - subtract
            - multiply
            - divide
        takes two parameters and returns result

---
+++ script.js
@@ -0,0 +1,20 @@
+// functions of the four basic mathematical operations
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    if (num2 === 0) {
+        return "undefined";
+    }
+    return num1 / num2;
+}
